38c6f03c2d5b80a58f66932bb2caa4b6b6343b50
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-08 14:33:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,8 @@
 # code your #position_taken? method here!
 def position_taken(board,index)
+  board_index = index.to_i - 1
   
+  if board_index == " "
+    return false
+  else if board_index == "X"
 end
